bbe336b354e9220ea2c8025e99e96c3313b00ab7
Weslei Freitas <weslei.rfreitas@gmail.com>
2016-05-03 16:36:10 -0300

    feat(app): atualizar formulário do representante

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,7 @@
 console.info('app.js init');
+var config = {
+  dsn: 'px_interblock_sql_local'
+}
 var app = angular.module('angularApp', ['ngAria', 'ngMaterial', 'ngRoute'], function() {});
 
 app.config(['$routeProvider', '$locationProvider', '$mdThemingProvider', '$mdDateLocaleProvider', function($routeProvider, $locationProvider, $mdThemingProvider, $mdDateLocaleProvider) {
@@ -13,16 +16,55 @@ app.config(['$routeProvider', '$locationProvider', '$mdThemingProvider', '$mdDat
     redirectTo: '/'
   });
 
+  moment.locale('pt-br');
   // https://material.angularjs.org/latest/api/service/$mdDateLocaleProvider
   $mdDateLocaleProvider.months = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro'];
   $mdDateLocaleProvider.shortMonths = ['jan.', 'fev', 'mar', 'abr', 'maio', 'jun', 'jul', 'ago', 'set', 'out', 'nov', 'dez'];
+  $mdDateLocaleProvider.parseDate = function(dateString) {
+    //moment.locale('pt-br');
+    var m = moment(dateString, 'L', true);
+    return m.isValid() ? m.toDate() : new Date(NaN);
+  };
+  $mdDateLocaleProvider.formatDate = function(date) {
+    //moment.locale('pt-br');    
+    if (moment(date).format('L') === 'Invalid date') return ''
+    else return moment(date).format('L');
+  };
+}]);
+
+app.factory('formService', ['$http', function($http) {
+
+  var service = {};
+  service.saveData = saveData;
+
+  return service;
+
+  function saveData(params, callback) {
+    params.dsn = config.dsn;
+    $http({
+      method: 'POST',
+      url: 'custom/representante/representante.cfc?method=saveData',
+      params: params
+    }).success(function(response) {
+      callback(response);
+    }).
+    error(function(data, status, headers, config) {
+      // Erro
+      alert('Ops! Ocorreu um erro inesperado.\nPor favor contate o administrador do sistema!');
+    });
+  }
 
 }]);
 
-app.controller('FormCtrl', function($scope) {
-  $scope.user = {
+
+app.controller('FormCtrl', ['formService', '$scope', function(formService, $scope) {
+
+  $scope.cliente = {
+    nascimento: '',
+    tipo: 'F',
     uf: 'SP'
-  };
+  }
+
   /*
   $scope.states = ('AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS ' +
   	'MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI ' +
@@ -32,6 +74,7 @@ app.controller('FormCtrl', function($scope) {
   	};
   });
   */
+
   $scope.equipamentos = [{
     nome: 'BLOQUEADOR AUTOMÁTICO',
     valorVista: '0,00',
@@ -112,5 +155,28 @@ app.controller('FormCtrl', function($scope) {
     entrada: '0,00',
     nParcelasEntrada: 1
   }
-})
+
+  $scope.contrato = {
+    dataInstalacao: '',
+    dataInicio: '',
+    dataTermino: '',
+    dataRenovacao: ''
+  }
+
+  $scope.saveData = function() {
+    alert();
+    var params = {
+      representante: $scope.representante,
+      cliente: $scope.cliente,
+      veiculo: $scope.veiculo,
+      equipamentos: angular.toJson($scope.equipamentos),
+      servicos: angular.toJson($scope.servicos),
+      pagamento: angular.toJson($scope.pagamento),
+      contrato: $scope.contrato
+    };
+    formService.saveData(params, function(response) {
+      console.info('response', response);
+    })
+  }
+}])
 console.info('app.js done');
